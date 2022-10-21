807f5a4026766a9d02c726282660f40c762625aa
Buzzi <Buzzi@FRED>
2022-10-21 14:38:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Area
+{
+    internal class Program
+    {
+        public class Circle
+        {
+            float _pi = 3.141f;
+            int _radius;
+            float _area;
+
+            public Circle(int Radius)
+            {
+                this._radius = Radius;
+            }
+
+            public void getArea()
+            {
+                _area = _radius * _radius * _pi;
+                Console.WriteLine("Arean på circeln är {0}",_area);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Circle rad5 = new Circle(5);
+            rad5.getArea();
+            Circle rad6 = new Circle(6);
+            rad6.getArea();
+        }
+    }
+}
